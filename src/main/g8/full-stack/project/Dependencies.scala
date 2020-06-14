d9dc84dd6042dd0f8a2306a4cab0796100c95dcd
Trond Bjerkestrand <trond.bjerkestrand@gmail.com>
2020-06-14 21:37:56 +0200

    Update monocle-core, monocle-macro to 2.0.5.

--- src/main/g8/full-stack/project/Dependencies.scala
+++ src/main/g8/full-stack/project/Dependencies.scala
@@ -3,7 +3,7 @@ object Version {
   val akka           = "2.6.6"
   val autowire       = "0.3.2"
   val boopickle      = "1.3.3"
-  val monocle        = "2.0.4"
+  val monocle        = "2.0.5"
   val scala          = "2.13.2"
   val scalajsDom     = "1.0.0"
   val scalajsScripts = "1.1.4"
